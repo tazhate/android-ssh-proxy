4dfa56c5aaffc19bede726ba7addf619dc873ed9
Guevara999 <cheguevara3727@gmail.com>
2026-08-07 20:11:28 +0300

    Delete app/src/main/java/com/example/sshproxy/ui directory

--- app/src/main/java/com/example/sshproxy/ui/splittunneling/AppInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sshproxy.ui.splittunneling
-
-data class AppInfo(
-    val packageName: String,
-    val appName: String,
-    val isSystem: Boolean,
-    val isSelected: Boolean
-)
