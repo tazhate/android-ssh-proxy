fe2cf09ccfa9f228cd957b9bdaec934977286c00
Guevara999 <cheguevara3727@gmail.com>
2026-08-09 01:50:19 +0300

    Update socket TrafficRouter.kt

--- app/src/main/java/com/example/sshproxy/network/TrafficRouter.kt
+++ app/src/main/java/com/example/sshproxy/network/TrafficRouter.kt
@@ -1,87 +1,71 @@
-package com.example.sshproxy.network
+fun start() {
+    Log.d(TAG, "TrafficRouter start() called")
+    isRunning = true
+    Thread {
+        try {
+            val inputStream = FileInputStream(tunFileDescriptor)
+            val outputStream = FileOutputStream(tunFileDescriptor)
+            val tunnelInput = tunnelSocket.getInputStream()
+            val tunnelOutput = tunnelSocket.getOutputStream()
 
-import android.net.VpnService
-import android.util.Log
-import java.io.FileDescriptor
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.net.Socket
+            Log.d(TAG, "TUN FD: $tunFileDescriptor, Socket: ${tunnelSocket.remoteSocketAddress}")
+            Log.d(TAG, "Starting router threads")
 
-class TrafficRouter(
-    private val vpnService: VpnService,
-    private val tunFileDescriptor: FileDescriptor,
-    private val tunnelSocket: Socket
-) {
-    private var isRunning = false
-    private val TAG = "TrafficRouter"
-
-    fun start() {
-        Log.d(TAG, "TrafficRouter start() called")
-        isRunning = true
-        Thread {
-            try {
-                val inputStream = FileInputStream(tunFileDescriptor)
-                val outputStream = FileOutputStream(tunFileDescriptor)
-                val tunnelInput = tunnelSocket.getInputStream()
-                val tunnelOutput = tunnelSocket.getOutputStream()
-                val buffer = ByteArray(32767)
-
-                Log.d(TAG, "TUN FD: $tunFileDescriptor, Socket: $tunnelSocket")
-                Log.d(TAG, "Starting router threads")
-
-                val readThread = Thread {
-                    Log.d(TAG, "Read thread started")
-                    while (isRunning) {
-                        try {
-                            val len = inputStream.read(buffer)
-                            if (len > 0) {
-                                tunnelOutput.write(buffer, 0, len)
-                                tunnelOutput.flush()
-                                if (Log.isLoggable(TAG, Log.DEBUG)) {
-                                    Log.d(TAG, "Wrote $len bytes to tunnel")
-                                }
+            val readThread = Thread {
+                Log.d(TAG, "Read thread started")
+                while (isRunning) {
+                    try {
+                        val len = inputStream.read(buffer)
+                        if (len > 0) {
+                            tunnelOutput.write(buffer, 0, len)
+                            tunnelOutput.flush()
+                            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                                Log.d(TAG, "Wrote $len bytes to tunnel")
                             }
-                        } catch (e: Exception) {
+                        }
+                    } catch (e: Exception) {
+                        if (isRunning) {
                             Log.e(TAG, "Read thread error", e)
-                            break
+                        } else {
+                            Log.d(TAG, "Read thread stopped due to isRunning = false")
                         }
+                        break
                     }
-                    Log.d(TAG, "Read thread stopped")
                 }
-
-                val writeThread = Thread {
-                    Log.d(TAG, "Write thread started")
-                    while (isRunning) {
-                        try {
-                            val len = tunnelInput.read(buffer)
-                            if (len > 0) {
-                                outputStream.write(buffer, 0, len)
-                                outputStream.flush()
-                                if (Log.isLoggable(TAG, Log.DEBUG)) {
-                                    Log.d(TAG, "Wrote $len bytes to TUN")
-                                }
-                            }
-                        } catch (e: Exception) {
-                            Log.e(TAG, "Write thread error", e)
-                            break
-                        }
-                    }
-                    Log.d(TAG, "Write thread stopped")
-                }
-
-                readThread.start()
-                writeThread.start()
-                readThread.join()
-                writeThread.join()
-
-            } catch (e: Exception) {
-                Log.e(TAG, "Router error", e)
+                Log.d(TAG, "Read thread stopped")
             }
-        }.start()
-    }
 
-    fun stop() {
-        Log.d(TAG, "TrafficRouter stop() called")
-        isRunning = false
-    }
+            val writeThread = Thread {
+                Log.d(TAG, "Write thread started")
+                while (isRunning) {
+                    try {
+                        val len = tunnelInput.read(buffer)
+                        if (len > 0) {
+                            outputStream.write(buffer, 0, len)
+                            outputStream.flush()
+                            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                                Log.d(TAG, "Wrote $len bytes to TUN")
+                            }
+                        }
+                    } catch (e: Exception) {
+                        if (isRunning) {
+                            Log.e(TAG, "Write thread error", e)
+                        } else {
+                            Log.d(TAG, "Write thread stopped due to isRunning = false")
+                        }
+                        break
+                    }
+                }
+                Log.d(TAG, "Write thread stopped")
+            }
+
+            readThread.start()
+            writeThread.start()
+            readThread.join()
+            writeThread.join()
+
+        } catch (e: Exception) {
+            Log.e(TAG, "Router error", e)
+        }
+    }.start()
 }
