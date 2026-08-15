2e560a2f0c5e1173fe29d439d42f11e38c4963e3
Guevara999 <cheguevara3727@gmail.com>
2026-08-16 02:31:55 +0300

    Update TProxyService.java

--- app/src/main/java/hev/sockstun/TProxyService.java
+++ app/src/main/java/hev/sockstun/TProxyService.java
@@ -1,10 +1,6 @@
 package hev.sockstun;
 
 public class TProxyService {
-    // IMPORTANT: The signature must match the .so exactly.
-    // The sockstun library uses boolean return, but some builds use void.
-    // We'll use void to match the error signature, then test.
-    // If you get a different error, switch to boolean.
     public static native void TProxyStartService(String configPath, int fd);
     public static native void TProxyStopService();
     public static native boolean TProxyIsRunning();
