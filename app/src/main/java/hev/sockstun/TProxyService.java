4155c1b386f2b889d7422b79977fe8276f244221
Guevara999 <cheguevara3727@gmail.com>
2026-08-16 00:47:24 +0300

    Update TProxyService.java

--- app/src/main/java/hev/sockstun/TProxyService.java
+++ app/src/main/java/hev/sockstun/TProxyService.java
@@ -1,11 +1,12 @@
 package hev.sockstun;
 
 public class TProxyService {
-    // Native methods from libhev-socks5-tunnel.so (sockstun version)
+    // Native method declarations
     public static native boolean TProxyStartService(String configPath, int fd);
     public static native boolean TProxyStopService();
     public static native boolean TProxyIsRunning();
 
+    // STATIC BLOCK – REQUIRED
     static {
         System.loadLibrary("hev-socks5-tunnel");
     }
