51f85e67e791889e02395a27db9f6d5a41bf6003
Guevara999 <cheguevara3727@gmail.com>
2026-08-16 05:23:43 +0300

    Update TProxyService.java

--- app/src/main/java/hev/sockstun/TProxyService.java
+++ app/src/main/java/hev/sockstun/TProxyService.java
@@ -1,16 +1,9 @@
 package hev.sockstun;
 
 public class TProxyService {
-    // Start the VPN tunnel (receives YAML config path and TUN file descriptor)
     public static native void TProxyStartService(String configPath, int fd);
-    
-    // Stop the VPN tunnel
     public static native void TProxyStopService();
-    
-    // Check if the tunnel is running
     public static native boolean TProxyIsRunning();
-    
-    // Get traffic statistics (returns long[]: [tx_bytes, tx_packets, rx_bytes, rx_packets])
     public static native long[] TProxyGetStats();
 
     static {
